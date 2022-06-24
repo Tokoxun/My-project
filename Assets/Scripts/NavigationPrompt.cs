adfee7753899aed5ae46b4691e486112d0d52690
unknown <khaw xun>
2022-06-24 18:52:50 +0800

    End of chapter 4

---
+++ Assets/Scripts/NavigationPrompt.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationPrompt : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.CompareTag("Borders"))
+        {
+            Debug.Log("Leave Town");
+        }
+    }
+}
