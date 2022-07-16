7a7f5e954da9af353fa868e867a3ef650c988250
unknown <khaw xun>
2022-07-16 22:36:04 +0800

    Some changes

--- Assets/Scripts/Classes/ConversationEntry.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class ConversationEntry
-{
-    public string SpeakingCharacterName;
-    public string ConversationText;
-    public Sprite DisplayPic;
-}
