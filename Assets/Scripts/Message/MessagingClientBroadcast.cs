110fdb4d6c68981fd83173b16721137d525e7a69
unknown <khaw xun>
2022-08-21 16:16:18 +0800

    Changes to mesgreceiver

--- Assets/Scripts/Message/MessagingClientBroadcast.cs
+++ Assets/Scripts/Message/MessagingClientBroadcast.cs
@@ -4,11 +4,17 @@ public class MessagingClientBroadcast : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        MessagingManager.Instance.Broadcast();
+        if (col.collider.tag == "ShopkeepConversation")
+        {
+            var receiver = GetComponent<MessagingClientReceiverMayorAndShopkeep>();
+            receiver.ThePlayerIsTryingSomething();
+        }
+        else if (col.collider.tag == "MayorConversation")
+        {
+            var receiverM = GetComponent<MessagingClientReceiverMayorAndShopkeep>();
+            receiverM.ThePlayerIsTryingToLeave();
+        }
     }
 
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        MessagingManager.Instance.Broadcast();
-    }
+
 }
---
+++ Assets/Scripts/Message/MessagingClientReceiverMayorAndShopkeep.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class MessagingClientReceiverMayorAndShopkeep : MonoBehaviour
+{
+    // void Start()
+    // {
+    //     MessagingManager.Instance.Subscribe(ThePlayerIsTryingToLeave);
+    // }
+
+    public void ThePlayerIsTryingToLeave()
+    {
+       var dialog = GetComponent<ConversationComponent>();
+       if (dialog != null)
+       {
+           if (dialog.CustomConversationUtility != null && dialog.CustomConversationUtility.Length > 0)
+            {
+               var conversation = dialog.CustomConversationUtility[0];
+                if (conversation != null)
+                {
+                    Debug.Log("conversation");
+                    ConversationManager.Instance.StartConversation(conversation);
+                }
+            }
+        }
+    }
+
+    public void ThePlayerIsTryingSomething()
+    {
+       var dialogS = GetComponent<ConversationComponent>();
+       if (dialogS != null)
+       {
+           if (dialogS.CustomConversationUtility != null && dialogS.CustomConversationUtility.Length > 0)
+            {
+               var conversationS = dialogS.CustomConversationUtility[1];
+                if (conversationS != null)
+                {
+                    Debug.Log("conversation");
+                    ConversationManager.Instance.StartConversation(conversationS);
+                }
+            }
+        }
+    }
+
+    // void OnDestroy()
+    // {
+    //     if (MessagingManager.Instance != null)
+    //     {
+    //         MessagingManager.Instance.UnSubscribe(ThePlayerIsTryingToLeave);
+    //     }
+    // }
+
+}
