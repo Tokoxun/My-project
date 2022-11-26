890a0877e74bef3911df001c9fa491909286ad18
unknown <khaw xun>
2022-11-26 16:20:34 +0800

    changes to jump

--- Assets/Scripts/CharacterJump.cs
+++ Assets/Scripts/CharacterJump.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using System.Threading;
-
 
 public class CharacterJump : MonoBehaviour
 {
@@ -16,10 +14,16 @@ public class CharacterJump : MonoBehaviour
     public float gravityScale = 5;
     private bool isJump = false;
     float velocity;
-
-    private Vector3 jumpPos;
+    private Vector2 jumpPos;
     static private float timeToJump = 0.10f;
     private float timer = 0f;
+    public float xspeed = 0.1f;
+    public float movePlayerHorizontal;
+    public float xvelocity;
+    public float xdistance = 0f;
+    // public float timeToDrop = 0.10f;
+    // private float droptimer = 0f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -32,28 +36,36 @@ public class CharacterJump : MonoBehaviour
     void Update()
     {
         velocity += gravity * gravityScale * Time.deltaTime;
+        movePlayerHorizontal = Input.GetAxis("Horizontal");
+        xvelocity = xspeed * movePlayerHorizontal;
         if (isJump && timer >= timeToJump)
         {
-            velocity = 0;
-            timer = 0;
-            isJump = false;
+            // velocity = 20;
+            // transform.Translate(new Vector2(xdistance, -velocity) * Time.deltaTime);
+            // droptimer = 0;
+            jumpPos += new Vector2(xdistance, 0);
             transform.position = jumpPos;
+            xdistance = 0f;
+            isJump = false;
+            timer = 0;
+            velocity = 0;
         }
         else if (isJump && timer < timeToJump)
         {
             timer += Time.deltaTime;
-            transform.Translate(new Vector3(0, velocity, 0) * Time.deltaTime);
+            transform.Translate(new Vector2(xvelocity, velocity) * Time.deltaTime);
+            xdistance += xvelocity;
         }
         else
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                jumpPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+                jumpPos = new Vector2(transform.position.x, transform.position.y);
                 isJump = true;
                 timer += Time.deltaTime;
 
                 velocity = jumpForce;
-                transform.Translate(new Vector3(0, velocity, 0) * Time.deltaTime);
+                transform.Translate(new Vector2(xvelocity, velocity) * Time.deltaTime);
             }
         }
     }
