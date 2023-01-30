eedd896fbe2461401f1aa800ad9712c1772f1174
Khaw Xun <xunkhaw2@gmail.com>
2023-01-30 17:29:40 +0800

    Small changes to detection zone

--- Assets/Scripts/DetectionZone.cs
+++ Assets/Scripts/DetectionZone.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class DetectionZone : MonoBehaviour
 {
+    private bool Awareness = false;
+
+
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        Debug.Log("Alerted");
+        Awareness = true;
+        Debug.Log(Awareness);
     }
 }
