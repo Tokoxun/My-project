464a7a390cb97c2ff312b8a8c6dc6521bde1e71e
Khaw Xun <xunkhaw2@gmail.com>
2023-02-18 16:47:13 +0800

    Cleaning up unused line of code in patrol

--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Patrol : MonoBehaviour{
-    static public float sideLength = 3.0f;
     static public float sideDuration = 3.0f; 
 
     private Rigidbody2D playerRigidBody2D; 
@@ -12,21 +11,16 @@ public class Patrol : MonoBehaviour{
     // Animator component for the player 
     private Animator playerAnim;
 
-    private Vector2 startCoordinate;
-    private Vector2[] patrolPoints;
 
     // Compute the translation vector for the next frame
     private int indexer() {
-        return (int)(Time.timeSinceLevelLoad/sideDuration) % patrolPoints.Length;
+        return (int)(Time.timeSinceLevelLoad/sideDuration) % 4;
     }
 
     void Awake() {
         playerRigidBody2D = (Rigidbody2D)GetComponent(typeof(Rigidbody2D));
         playerAnim = (Animator)GetComponent(typeof(Animator));
         playerSpriteImage =(SpriteRenderer)GetComponent(typeof(SpriteRenderer));
-
-        Vector2 startCoordinate = new Vector2(transform.position.x, transform.position.y);
-        patrolPoints = new Vector2[] {startCoordinate, startCoordinate + new Vector2(0.0f, -3.0f), startCoordinate + new Vector2(3.0f, -3.0f), startCoordinate + new Vector2(3.0f, 0.0f)};
     }
 
     void Update() {
