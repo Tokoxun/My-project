723e2b53233ef957006562f54dd95aa41411862f
Khaw Xun <xunkhaw2@gmail.com>
2023-03-14 23:24:29 +0800

    Implemented ranged enemy distancing from the player

--- Assets/Scripts/RangeDetectionZone.cs
+++ Assets/Scripts/RangeDetectionZone.cs
@@ -7,14 +7,16 @@ public class RangeDetectionZone : MonoBehaviour
     private Animator AIAnim;
     private SpriteRenderer AISpriteImage;
     public float AIspeed = 2f;
-    private float ProfAndAIDistx;
+    private float ProfAndAIDist;
     private float ProfAndAIDisty;
     private bool playerNotDetected = true;
     private GameObject detection;
     private bool attacks = false;
     private GameObject range;
     public Transform professor;
-
+    private float timeToAvoid;
+    private float AvoidCooldown = 3f;
+    private float AvoidDist = 6.5f;
 
     void Start()
     {
@@ -24,6 +26,7 @@ public class RangeDetectionZone : MonoBehaviour
         detection.SetActive(playerNotDetected);
         AIAnim=(Animator)GetComponent(typeof(Animator));
         AISpriteImage =(SpriteRenderer)GetComponent(typeof(SpriteRenderer));
+        timeToAvoid = AvoidCooldown;
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
@@ -38,10 +41,28 @@ public class RangeDetectionZone : MonoBehaviour
     {
         int profposy = Mathf.RoundToInt(professor.transform.position.y);
         int AIposy = Mathf.RoundToInt(transform.position.y);
-        ProfAndAIDistx = professor.transform.position.x - transform.position.x;
-        ProfAndAIDisty = professor.transform.position.y - transform.position.y;
+        ProfAndAIDist = Vector2.Distance(professor.transform.position, transform.position);
+        ProfAndAIDisty = transform.position.y - professor.transform.position.y;
         if(playerNotDetected == false)
         {
+            timeToAvoid += Time.deltaTime;
+            if(ProfAndAIDist <= 5 && timeToAvoid >= AvoidCooldown)
+            {    
+                if(professor.transform.position.x > transform.position.x)
+                {
+                    transform.Translate(new Vector2(transform.position.x - AvoidDist, 0) * Time.deltaTime);
+                }
+
+                if(professor.transform.position.x < transform.position.x)
+                {
+                    transform.Translate(new Vector2(transform.position.x + AvoidDist, 0) * Time.deltaTime);
+                }
+            }
+
+            if(ProfAndAIDist > 5 && timeToAvoid >= AvoidCooldown)
+            {
+                timeToAvoid = 0;
+            }
             // transform.Translate(new Vector2(ProfAndAIDistx * AIspeed, ProfAndAIDisty * AIspeed) * Time.deltaTime);
         }
     }
