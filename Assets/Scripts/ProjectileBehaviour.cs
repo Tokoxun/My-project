82f23b33fecdea6eda33eccd1f3d880359d296f9
Khaw Xun <xunkhaw2@gmail.com>
2023-03-20 18:38:00 +0800

    Added timer to arrow shots to despawn if not hit

--- Assets/Scripts/ProjectileBehaviour.cs
+++ Assets/Scripts/ProjectileBehaviour.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class ProjectileBehaviour : MonoBehaviour
 {
     private float speed = 5f;
+    private float timeBeforeDespawn;
+    private float despawnTime = 2f;
+
     private void Update()
     {
+        timeBeforeDespawn += Time.deltaTime;
         transform.position += -transform.right * Time.deltaTime * speed;
     }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.collider.tag == "Ally")
+        if (col.gameObject.tag == "Ally")
         {
             Destroy(gameObject);
+            timeBeforeDespawn = 0;
+            Debug.Log("Hit");
+        }
+        if(timeBeforeDespawn >= despawnTime)
+        {
+            Destroy(gameObject);
+            timeBeforeDespawn = 0;
         }
     }
 }
