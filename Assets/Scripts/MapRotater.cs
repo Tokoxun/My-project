ded5b0c59f9c7fb24d1c2b7299109e8f183a8efb
Khaw Xun <xunkhaw2@gmail.com>
2023-03-27 21:41:05 +0800

    Added new scene for map rotating

--- Assets/Scripts/MapRotater.cs
+++ Assets/Scripts/MapRotater.cs
@@ -2,32 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class MapRotater
-{
-    public static Dictionary<string, string> RouteInformation = new Dictionary<string, string>()
-    {
-        {"Overworld", "The big bad world", CanTravel = true}, {"Construction", "The construction area", CanTravel = false},;
-    }
+// public static class MapRotater
+// {
+//     public static Dictionary<string, string> RouteInformation = new Dictionary<string, string>()
+//     {
+//         {"Overworld", "The big bad world", CanTravel = true}, {"Construction", "The construction area", CanTravel = false};
+//     }
 
-    public static string GetRouteInfo(string, destination)
-    {
-        public static string GetRouteInfo(string, destination)
-        {
-            return RouteInformation.ContainsKey(destination) ? RouteInformation[destination] : null;
-        }
-    }
+//     public static string GetRouteInfo(string, destination)
+//     {
+//         public static string GetRouteInfo(string, destination)
+//         {
+//             return RouteInformation.ContainsKey(destination) ? RouteInformation[destination] : null;
+//         }
+//     }
 
-    public static bool CanNavigate(string, destination)
-    {
-        return RouteInformation.ContainsKey(destination) ? RouteInformation[destination].CanTravel : false;
-    }
-    public static void NavigateTo(string, destination)
-    {
+//     public static bool CanNavigate(string, destination)
+//     {
+//         return RouteInformation.ContainsKey(destination) ? RouteInformation[destination].CanTravel : false;
+//     }
+//     public static void NavigateTo(string, destination)
+//     {
 
-    }
-    public struct Route
-    {
-        public string RouteDescription;
-        public bool CanTravel;
-    }
-}
+//     }
+//     public struct Route
+//     {
+//         public string RouteDescription;
+//         public bool CanTravel;
+//     }
+// }
--- Assets/Scripts/Navigation/NavigationPrompt.cs
+++ Assets/Scripts/Navigation/NavigationPrompt.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NavigationPrompt : MonoBehaviour
-{
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        if(MapRotater.CanNavigate(this.tag))
-        {
-            Debug.Log("attempting to exit" + tag);
-            MapRotater.NavigateTo(this.tag);
-        }
-    }
-}
+// public class NavigationPrompt : MonoBehaviour
+// {
+//     void OnCollisionEnter2D(Collision2D col)
+//     {
+//         if(MapRotater.CanNavigate(this.tag))
+//         {
+//             Debug.Log("attempting to exit" + tag);
+//             MapRotater.NavigateTo(this.tag);
+//         }
+//     }
+// }
