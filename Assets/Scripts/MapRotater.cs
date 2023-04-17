cf724855c9b84a03cef5eb79943a155a50a56b55
Khaw Xun <xunkhaw2@gmail.com>
2023-04-17 22:45:55 +0800

    Enemy wave cleared UI display functional and will transistion to different map on a timer

--- Assets/Scripts/MapRotater.cs
+++ Assets/Scripts/MapRotater.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MapRotater : MonoBehaviour
 {
+    public CanvasGroup dialogBox;
     private string sceneName;
     public string[] scenes;
     private float EnemyCount;
     private float enemyDied;
+    private float transistionTime = 0.5f;
+    private float mapTransistion;
+    private bool cleared = false;
 
     void Start()
     {
@@ -30,8 +35,27 @@ public class MapRotater : MonoBehaviour
         Debug.Log(enemyDied);
         if(enemyDied == EnemyCount)
         {
-            sceneName = scenes[Random.Range(0, scenes.Length)];
-            SceneManager.LoadScene(sceneName);
+            dialogBox.alpha = 1;
+            dialogBox.blocksRaycasts = true;
+            cleared = true;
+        }
+    }
+
+    void Update()
+    {
+        if(cleared)
+        {
+            mapTransistion += Time.deltaTime;
+            if(mapTransistion >= transistionTime)
+            {
+                dialogBox.alpha = 0;
+                dialogBox.blocksRaycasts = false;
+                sceneName = scenes[Random.Range(0, scenes.Length)];
+                SceneManager.LoadScene(sceneName);
+                mapTransistion = 0;
+                enemyDied = 0;
+                cleared = false;
+            }
         }
     }
 }
