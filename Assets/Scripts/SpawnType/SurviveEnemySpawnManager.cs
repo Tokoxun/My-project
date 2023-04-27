4f0d5ebb8745522e529510f7e7ff32800a7e0600
Khaw Xun <xunkhaw2@gmail.com>
2023-04-27 22:45:25 +0800

    Started to create enemy spawn types

---
+++ Assets/Scripts/SpawnType/SurviveEnemySpawnManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurviveEnemySpawnManager : MonoBehaviour
+{
+    public float spawnTime = 5f;
+    //The amount of time between each spawn
+    public float spawnDelay = 3f;
+    //The amount of time before spawning starts
+    public GameObject[] enemies;
+    //Array of enemy prefabs
+    private float surviveTimer = 0;
+    private float TimeOut = 60f;
+
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnDelay, spawnTime);
+    }
+
+    void Spawn()
+    {
+        //Instantiate a random enemy
+        int enemyIndex = Random.Range(0, enemies.Length);
+        Instantiate(enemies[enemyIndex], transform.position, transform.rotation);
+    }
+
+    void Update()
+    {
+        surviveTimer += Time.deltaTime;
+        if(surviveTimer >= TimeOut)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
