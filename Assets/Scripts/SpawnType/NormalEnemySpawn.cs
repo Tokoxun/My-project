0d17669b0238a540756e6d4bacccba20f5a6b5f1
Khaw Xun <xunkhaw2@gmail.com>
2023-05-01 22:34:26 +0800

    Added new type of spawn

---
+++ Assets/Scripts/SpawnType/NormalEnemySpawn.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalEnemySpawn : MonoBehaviour
+{
+    public float spawnTime = 5f;
+    //The amount of time between each spawn
+    public float spawnDelay = 3f;
+    //The amount of time before spawning starts
+    public GameObject[] enemies;
+    //Array of enemy prefabs
+    public int EnemyIndex = 0;
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnDelay, spawnTime);
+    }
+
+    void Spawn()
+    {
+        if(EnemyIndex == enemies.Length)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instantiate(enemies[EnemyIndex], transform.position, transform.rotation);
+            EnemyIndex += 1;
+        }
+        
+    }
+}
