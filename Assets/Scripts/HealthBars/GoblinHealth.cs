eae3cef130ce1af991564cb26991c6e227d93634
Khaw Xun <xunkhaw2@gmail.com>
2023-05-03 23:48:28 +0800

    Separating script to folder

---
+++ Assets/Scripts/HealthBars/GoblinHealth.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinHealth : MonoBehaviour
+{
+    [SerializeField] private int health = 100;
+
+    // private int MAX_HEALTH = 100;
+
+
+    public void Damage(int amount)
+    {
+        MapRotater enemyLeft = GetComponentInParent<MapRotater>();
+        if(amount < 0)
+        {
+            throw new System.ArgumentOutOfRangeException("Cannot have negative Damage");
+        }
+
+        this.health -= amount;
+        Debug.Log(this.health);
+
+        if(this.health <= 0)
+        {
+            enemyLeft.EnemyKilled();
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Debug.Log("I am Dead!");
+        Destroy(gameObject);
+    }
+}
