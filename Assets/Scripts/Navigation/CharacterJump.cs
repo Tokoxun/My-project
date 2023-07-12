46f662a59d74dacce36d5d39e6890bffb9d53942
Khaw Xun <xunkhaw2@gmail.com>
2023-07-12 22:01:45 +0800

    Fixed a issue where player wont jump when jump button is pressed

--- Assets/Scripts/Navigation/CharacterJump.cs
+++ Assets/Scripts/Navigation/CharacterJump.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using EasyJoystick;
 
 public class CharacterJump : MonoBehaviour
 {
@@ -13,6 +13,7 @@ public class CharacterJump : MonoBehaviour
     public float gravity = -9.81f;
     public float gravityScale = 5;
     private bool isJump = false;
+    private bool jumped = false;
     float velocity;
     private Vector2 jumpPos;
     static private float timeToJump = 0.10f;
@@ -23,6 +24,7 @@ public class CharacterJump : MonoBehaviour
     public float xdistance = 0f;
     public float timeToDrop = 0.10f;
     private float droptimer = 0f;
+    public Joystick joystick;
 
 
     // Start is called before the first frame update
@@ -32,51 +34,55 @@ public class CharacterJump : MonoBehaviour
         profSpriteImage =(SpriteRenderer)GetComponent(typeof(SpriteRenderer));
     }
 
-
     void Update()
     {
-        velocity += gravity * gravityScale * Time.deltaTime;
-        movePlayerHorizontal = Input.GetAxis("Horizontal");
-        xvelocity = xspeed * movePlayerHorizontal;
-        if (isJump && timer >= timeToJump)
-        {
-            droptimer += Time.deltaTime;
-            if (droptimer < timeToDrop)
-            {
-                velocity = 20;
-                transform.Translate(new Vector2(xvelocity, -velocity) * Time.deltaTime);
-                xdistance += xvelocity;
-            }
-            else
-            {
-                jumpPos += new Vector2(xdistance, 0);
-                transform.position = jumpPos;
-                xdistance = 0f;
-                isJump = false;
-                timer = 0;
-                velocity = 0;
-                droptimer = 0;
-            }
-            
-        }
-        else if (isJump && timer < timeToJump)
+        if(jumped)
         {
             timer += Time.deltaTime;
-            transform.Translate(new Vector2(xvelocity, velocity) * Time.deltaTime);
-            xdistance += xvelocity;
-        }
-        else
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (isJump && timer >= timeToJump)
             {
-                jumpPos = new Vector2(transform.position.x, transform.position.y);
-                isJump = true;
+                droptimer += Time.deltaTime;
+                if (droptimer < timeToDrop)
+                {
+                    velocity = 20;
+                    transform.Translate(new Vector2(xvelocity, -velocity) * Time.deltaTime);
+                    xdistance += xvelocity;
+                }
+                else
+                {
+                    jumpPos += new Vector2(xdistance, 0);
+                    transform.position = jumpPos;
+                    xdistance = 0f;
+                    isJump = false;
+                    timer = 0;
+                    velocity = 0;
+                    droptimer = 0;
+                    jumped = false;
+                }
+            
+            }
+            else if (isJump && timer < timeToJump)
+            {
                 timer += Time.deltaTime;
-
-                velocity = jumpForce;
                 transform.Translate(new Vector2(xvelocity, velocity) * Time.deltaTime);
+                xdistance += xvelocity;
             }
         }
+    }
+
+
+    public void jumpButton()
+    {
+        velocity += gravity * gravityScale * Time.deltaTime;
+        movePlayerHorizontal = joystick.Horizontal();
+        xvelocity = xspeed * movePlayerHorizontal;
+        
+        jumpPos = new Vector2(transform.position.x, transform.position.y);
+        isJump = true;
+
+        velocity = jumpForce;
+        transform.Translate(new Vector2(xvelocity, velocity) * Time.deltaTime);
+        jumped = true;
     }
 }
 
