6a94e2333d17bc1837d91ce136436acb6b709012
Khaw Xun <xunkhaw2@gmail.com>
2023-08-29 22:51:52 +0800

    Removed Debug.Log

--- Assets/Scripts/Action/AttackArea.cs
+++ Assets/Scripts/Action/AttackArea.cs
@@ -7,11 +7,6 @@ public class AttackArea : MonoBehaviour
     // Start is called before the first frame update
     public int damage = 5;
     
-    void Update()
-    {
-        Debug.Log(damage);
-    }
-
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if(collider.GetComponent<GoblinHealth>() != null)
