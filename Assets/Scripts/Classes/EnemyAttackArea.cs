d5a716d32031abf4a4b4b5004976b7eece609fb7
Khaw Xun <xunkhaw2@gmail.com>
2023-10-11 16:20:51 +0800

    Enemy will stop moving when player collided with enemy attack range

--- Assets/Scripts/Classes/EnemyAttackArea.cs
+++ Assets/Scripts/Classes/EnemyAttackArea.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnemyAttackArea : MonoBehaviour
 {
+    private float stoppingTime = 1f;
+    private float timeStoped = 0f;
+    private bool haveStoped = false;
     // Start is called before the first frame update
     public float damage = 3f;
 
@@ -13,10 +16,32 @@ public class EnemyAttackArea : MonoBehaviour
     }
     public void OnTriggerEnter2D(Collider2D collider)
     {
+        if(collider.gameObject.tag == "Ally")
+        {
+            GoblinMovement stopMove = GetComponentInParent<GoblinMovement>();
+            stopMove.ChasingPlayer = false;
+            haveStoped = true; 
+        }
         if(collider.GetComponent<Health>() != null)
         {
             Health health = collider.GetComponent<Health>();
             health.Damage(damage);
+        }
+    }
+
+    void Update()
+    {
+        Debug.Log(haveStoped);
+        if(haveStoped)
+        {
+            timeStoped += Time.deltaTime;
+            if(timeStoped >= stoppingTime)
+            {
+                timeStoped = 0;
+                GoblinMovement stopMove = GetComponentInParent<GoblinMovement>();
+                stopMove.ChasingPlayer = true;
+                haveStoped = false;
+            }
         }
     }
 }
