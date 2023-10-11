3c0cfad30375e63b8f6138458f55325107e4f9c2
Khaw Xun <xunkhaw2@gmail.com>
2023-10-11 16:17:35 +0800

    Removed movement in detection zone to goblin movement

--- Assets/Scripts/DetectionZone.cs
+++ Assets/Scripts/DetectionZone.cs
@@ -4,100 +4,24 @@ using UnityEngine;
 
 public class DetectionZone : MonoBehaviour
 {
-    private Animator AIAnim;
-    private SpriteRenderer AISpriteImage;
-    public float AIspeed = 2f;
-    private float ProfAndAIDistx;
-    private float ProfAndAIDisty;
-    public bool ChasingPlayer = false;
+    public GoblinMovement goblinMove;
     private GameObject detection = default;
-    private Transform professor;
-    public Transform AttackAreaColliderRotate;
-    public Transform DetectionColliderRotate;
-    private Rigidbody2D rb;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        detection = transform.GetChild(1).gameObject;
-        detection.SetActive(true);
-        AIAnim=(Animator)GetComponent(typeof(Animator));
-        AISpriteImage =(SpriteRenderer)GetComponent(typeof(SpriteRenderer));
+        detection = this.gameObject;
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.tag == "Ally")
         {
-            professor = collider.transform;
-            ChasingPlayer = true;
+            goblinMove.professor = collider.transform;
+            goblinMove.ChasingPlayer = true;
+            goblinMove.playerSpotted = true;
             detection.SetActive(false);
+            SquarePatrol stopPatrol = GetComponentInParent<SquarePatrol>();
+            stopPatrol.enabled = false;
         }
     }
-
-    void FixedUpdate()
-    {
-        if(ChasingPlayer == true)
-        {
-            ProfAndAIDistx = professor.transform.position.x - transform.position.x;
-            ProfAndAIDisty = professor.transform.position.y - transform.position.y;
-            float distanceToPlayer = Vector2.Distance(transform.position, professor.position);
-            int profposy = Mathf.RoundToInt(professor.transform.position.y);
-            int AIposy = Mathf.RoundToInt(transform.position.y);
-            // transform.Translate(new Vector2(ProfAndAIDistx * AIspeed, ProfAndAIDisty * AIspeed) * Time.deltaTime);
-            Vector2 directionToPlayer = (professor.position - transform.position).normalized;
-            
-            // Move the enemy towards the player.
-            rb.velocity = directionToPlayer * AIspeed;
-            if(AIposy != profposy)
-            {
-                AIAnim.SetBool("walkHorizontal", false);
-                AISpriteImage.flipX = false;
-            
-                if(ProfAndAIDisty > 0)
-                {
-                    AIAnim.SetBool("walkUp", true);
-                    AIAnim.SetBool("walkDown", false);
-                    AttackAreaColliderRotate.transform.rotation = Quaternion.Euler(0, 0, 180);
-                    DetectionColliderRotate.transform.rotation = Quaternion.Euler(0, 0, 270);
-                }
-                else if(ProfAndAIDisty < 0)
-                {
-                    AIAnim.SetBool("walkDown", true);
-                    AIAnim.SetBool("walkUp", false);
-                    AttackAreaColliderRotate.transform.rotation = Quaternion.Euler(0, 0, 0);
-                    DetectionColliderRotate.transform.rotation = Quaternion.Euler(0, 0, 270);
-                }
-            }
-
-            else
-            {
-                AIAnim.SetBool("walkUp", false);
-                AIAnim.SetBool("walkDown", false);
-
-                if(ProfAndAIDistx > 0)
-                {
-                    AIAnim.SetBool("walkHorizontal", true);
-                    AISpriteImage.flipX = true;
-                    AttackAreaColliderRotate.transform.rotation = Quaternion.Euler(0, 0, 90);
-                    DetectionColliderRotate.transform.rotation = Quaternion.Euler(0, 0, 270);
-                }
-                else if (ProfAndAIDistx < 0)
-                {
-                    AIAnim.SetBool("walkHorizontal", true);
-                    AISpriteImage.flipX = false;
-                    AttackAreaColliderRotate.transform.rotation = Quaternion.Euler(0, 0, 270);
-                    DetectionColliderRotate.transform.rotation = Quaternion.Euler(0, 0, 270);
-                }
-                else
-                {
-                    AIAnim.SetBool("walkHorizontal", false);
-                }
-            }
-        }
-    }
-
-
-
-
 }
